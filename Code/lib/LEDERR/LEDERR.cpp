a69322648cc7cdd5871ab1fd3c0d1c8ddf9dbf3b
m241dan <korisd@gmail.com>
2017-02-26 19:20:53 -0500

    Update LEDERR.cpp

--- Code/lib/LEDERR/LEDERR.cpp
+++ Code/lib/LEDERR/LEDERR.cpp
@@ -1,23 +1,27 @@
 #include "LEDERR.h"
 
 //<<constructor>> 
-LEDERR::LEDERR(){
+LEDERR::LEDERR()
+{
 }
 
 //<<destructor>>
-LEDERR::~LEDERR(){
+LEDERR::~LEDERR()
+{
 }
 
 //Setup the Led pins
-void LEDERR::setUpLed(byte ledPins[]){
-   for (int i = 0; i <= sizeof(ledPins); i++){
+void LEDERR::setUpLed(byte ledPins[])
+{
+   for (int i = 0; i <= sizeof(ledPins); i++)
       pinMode(ledPins[i], OUTPUT);
-   }
 }
 
 //Flash the Led/Leds
-void LEDERR::flashLed(byte ledPins[], int duration){
-   for (int i = 0; i <= sizeof(ledPins); i++){
+void LEDERR::flashLed(byte ledPins[], int duration)
+{
+   for (int i = 0; i <= sizeof(ledPins); i++)
+   {
       digitalWrite(ledPins[i], HIGH);
       delay(duration/2);
       digitalWrite(ledPins[i], LOW);
@@ -25,16 +29,16 @@ void LEDERR::flashLed(byte ledPins[], int duration){
    }
 }
 
-void LEDERR::turnOn(byte ledPins[]){
-   for(int i = 0; i <= sizeof(ledPins); i++){
+void LEDERR::turnOn(byte ledPins[])
+{
+   for(int i = 0; i <= sizeof(ledPins); i++)
       digitalWrite(ledPins[i], HIGH);
-   }
 }
 
-void LEDERR::turnOff(byte ledPins[]){
-   for(int i = 0; i <= sizeof(ledPins); i++){
+void LEDERR::turnOff(byte ledPins[])
+{
+   for(int i = 0; i <= sizeof(ledPins); i++)
       digitalWrite(ledPins[i], LOW);
-   }
 }
 
 
