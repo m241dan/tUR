e6c730ac24c9291e050c6fc6fe8b9a9d23454c34
m241dan <korisd@gmail.com>
2017-03-05 15:28:34 -0500

    shifted somet stuff around to make it arduino compatible

---
+++ Code/libraries/SPEC/specSensor.h
@@ -0,0 +1,28 @@
+#ifndef spec_sensor_h
+#define spec_sensor_h
+
+#include "Arduino.h"
+
+typedef enum : byte
+{
+   SPEC_SO2, SPEC_O3, SPEC_NO2
+} specType;
+
+class Spec
+{
+   public:
+      Spec( specType s_type, int g, int r, int t );
+      ~Spec();
+
+      String generateReading();
+
+   private:
+      //Vars
+      specType type;
+      int vgas_pin;
+      int vref_pin;
+      int vtmp_pin;
+
+};
+
+#endif
---
+++ Code/libraries/SPEC/tests/spec.ino.ino
@@ -0,0 +1,11 @@
+#include "Spec.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
