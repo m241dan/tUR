318437abcf06773dacc56fd84a8f741c1bdda942
m241dan <korisd@gmail.com>
2017-04-12 21:53:36 -0400

    this is a nasty commit, but BMP library, pressure_test_ambient, initial master outline

--- Code/HASP/GOAT/GOAT.ino
+++ Code/HASP/GOAT/GOAT.ino
@@ -1,1 +1,47 @@
+/*
+ * HASP GOAT: Master Arduino
+ * Author: Daniel R. Koris
+ * Goal of Program: Issuing commands and receiving readings from the Slave Arduino,
+ *                  receiving commands and telemtry from HASP Gondola,
+ *                  taking readings from one half of the sensor rig,
+ *                  control the Sparkfun pump,
+ *                  and thermal controlling.
+ * Required Components: Arduino Mega, Adafruit MicroSD Card Breakout Board, Adafruit BME280,
+ *                      Spec SO2, Spec O3, Spec NO2, Adafruit Oscillator, Resistor Heaters,
+ *                      Thermoelectric Modules
+ */
 
+//THIS IS MOSTLY PSEUDO-CODE
+//THIS IS MOSTLY PSEUDO-CODE
+//THIS IS MOSTLY PSEUDO-CODE
+//THIS IS MOSTLY PSEUDO-CODE
+//THIS IS MOSTLY PSEUDO-CODE
+
+#include "Spec.h"
+#include <Adafruit_BME280.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+#include <Adafruit_SI5351.h>
+
+//sensor rig
+
+Spec so2( SPEC_O2, pin, pin, pin );
+Spec no2( SPEC_NO2, pin, pin, pin );
+Spec o3( SPEC_O3, pin, pin, pin );
+Adafruit_BME280 bme( pin );
+Adafruit_SI5351 clockgen();
+File hasp_log1;
+File hasp_log2;
+
+//
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
--- Code/HASP/pressure_test_ambient/pressure_test_ambient.ino
+++ Code/HASP/pressure_test_ambient/pressure_test_ambient.ino
@@ -8,26 +8,28 @@
 
 #include <SPI.h>
 #include <SD.h>
-#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP183.h>
 
 /****************************
  * Config / Globals Section *
  ****************************/
 
 //Dynamic Pins
-#define BME_PIN	53
+#define BMP_PIN	53
 #define SD_PIN 49
 
 //Sample Rate
 #define SAMPLE_RATE 1 		//In Hz, this basically just sets our delay between readings
 
-Adafruit_BME280 bme( BME_PIN );		//Handler for the BME sesnor, setup for hardwire SPI using an software CS pin
+Adafruit_BMP183 bmp( BMP_PIN );		//Handler for the BME sesnor, setup for hardwire SPI using an software CS pin
 File hasp_log;				//This is the handle for the file we will open
-float temperature, pressure, humidity;	//In a regular program, this would urk me, but I don't want the stack to reallocate everytime it loops
+float temperature, pressure;	//In a regular program, this would urk me, but I don't want the stack to reallocate everytime it loops
 int time_since_start;
 int file_iteration;
 String buf;
 String log_name;
+
 void setup()
 {
    file_iteration = 0;
@@ -35,9 +37,6 @@ void setup()
    Serial.begin( 9600 );		//standard setup for baudrate
    while( !Serial );  			//waiting for serial to connect (not really needed when not hooked to laptop)
 
-   Serial1.begin( 9600 );   //serial for bluetooth
-   while( !Serial1 );       //wait for serial to initialize
-      
    if( !SD.begin( SD_PIN ) ) 		//this uses the static implementation of the SD library
    {
       Serial.println( "Initialization of SD failed." );
@@ -45,12 +44,12 @@ void setup()
    }
    Serial.println( "SD Initialized." );
 
-   if( !bme.begin() )			//This initializes the sensor with default x16 sampling rates, no high-pass filter, normal mode, .5 delay between readings
+   if( !bmp.begin() )			//This initializes the sensor with default x16 sampling rates, no high-pass filter, normal mode, .5 delay between readings
    {
-      Serial.println( "Initialization of BME280 failed." );
+      Serial.println( "Initialization of BMP183 failed." );
       return;
    }
-   Serial.println( "BME280 initialized" );
+   Serial.println( "BMP183 initialized" );
 
    time_since_start = 0;
    log_name = getNextFile();
@@ -61,21 +60,18 @@ void loop()
    delay( SAMPLE_RATE * 1000 );    //Delaying basically one second because I don't want to overflow the SD and the BME usually needs a bit of additional time to "warm" up
 
    hasp_log = SD.open( log_name.c_str(), FILE_WRITE );
+   if( !hasp_log )
+      Serial.println( "SD not working..." );
       
    time_since_start += 1;		//This is a weak tracker of time since the program started
-   temperature = 0.0;			//Since these are globals, let's make sure we zero them before we use them, incase the sensor fails to read
-   humidity = 0.0;			//ibid
    pressure = 0.0;			//ibid
 					//A sample program divided this reading by 100.0F, so I am too
-   pressure = bme.readPressure() / 100.0F;
-   temperature = bme.readTemperature();
-   humidity = bme.readHumidity();
+   pressure = bmp.getPressure() / 100.0F;
+   temperature = bmp.getTemperature();
 
-
-   buf = String( time_since_start ) + " " + String( pressure ) + " " + String( temperature ) + " " + String( humidity );
-
+   buf = String( time_since_start ) + " " + String( pressure ) + " " + String( temperature );
+   Serial.println( buf ); 
    hasp_log.println( buf.c_str() );
-   Serial1.println( buf.c_str() );
    hasp_log.close();
 }
 
