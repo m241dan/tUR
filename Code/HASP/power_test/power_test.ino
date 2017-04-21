f80797e572d5abd72315886267bebc6443a5fa12
m241dan <korisd@gmail.com>
2017-04-21 16:04:07 -0400

    added this program, this will be what we run to get our accurate power draw

---
+++ Code/HASP/power_test/power_test.ino
@@ -0,0 +1,16 @@
+/*
+ *  HASP GOAT: Power Test
+ *  Author: Daniel R. Koris
+ */
+
+void setup()
+{
+  // put your setup code here, to run once:
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+}
