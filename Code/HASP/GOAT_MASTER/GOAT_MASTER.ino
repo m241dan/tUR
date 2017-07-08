40dc5b19b40b5857f5a1e168e14caa115e491d58
Daniel R. Koris <korisd@gmail.com>
2017-07-08 15:47:09 -0700

    still cruising along

--- Code/HASP/GOAT_MASTER/GOAT_MASTER.ino
+++ Code/HASP/GOAT_MASTER/GOAT_MASTER.ino
@@ -18,6 +18,7 @@
 #include "master_globals.h"
 #include "goat_funcs.h"
 #include "pump_controller.h"
+#include "state.h"
 
 
 struct sensor_table
@@ -85,6 +86,19 @@ struct data_set
     
 } sample_set;
 
+state state_machine[MAX_STATE] = { 
+                                   receive_ground( ground_command_handle, readings.gtp, Serial, buffers.ground, buffers.ground_index ),
+                                   receive_slave( readings.slave, Serial1, buffers.slave, buffers.slave_index ),
+                                   downlink_ground( readings, sample_set, statuss, timers, Serial, blu_serial ),
+                                   request_slave_reading( Serial1 ),
+                                   command_handler( ground_command_handle ),
+                                   timer_handler( timers, readings.gtp, statuss, pump ),
+                                   sample( sample_set, sensors )                                                 
+};
+
+STATE_ID current_state;
+
+
 void setupMasterSerials( void )
 {    //Serial to HASP
     Serial.begin( 1200 );
@@ -101,7 +115,8 @@ void setupMasterSerials( void )
 
 void setupMasterGlobals( void )
 {
-
+    timers.pump_timer = millis() + ( 30 * 1000 ); //Start the pump 30 seconds after start
+    timers.downlink_schedule = millis() + ( 20 * 1000 ); //Downlink the first reading 20 seconds from this time
 }
 
 void setupMasterSensors( void )
@@ -128,222 +143,112 @@ void setupMasterSensors( void )
         statuss.am2315_status = "AIGD";
 }
 
+/*
+ * I know, it's a dumb one-liner but it used to be more complicated lol
+ */
+void initStateMachine()
+{
+    current_state = TIMER_HANDLER;
+}
+
+/*
+ * This is a very important function. This function handles the state transition
+ * if a state does not return a recommended transition. IE if state.run() returns
+ * NONE_SPECIFIC. 
+ */
+STATE_ID determineTransition()
+{
+    STATE_ID transition = TIMER_HANDLER;
+
+    if( ground_serial.available() )
+       transition = RECEIVE_GROUND;
+    else if( slave_serial.available() )
+       transition = RECEIVE_SLAVE;
+    
+    return transition;
+}
 
 void setup()
 {
     setupMasterSerials();
+    Serial.println( "Master starting..." );
     setupMasterGlobals();
     setupMasterSensors();
-    
-    Serial2.begin( 9600 );
-    while( !Serial2 );
-    
-    delay( 2000 );
-  //  assignEntry( master_reading.time, C_TIME(), sizeof( master_reading.time ) );
-    sendData( Serial, (byte *)&master_reading, sizeof( master_reading ) );
-    sendData( Serial2, (byte *)&master_reading, sizeof( master_reading ) );
-    //block until we get a response from slave
-    while( !Serial1.available() );
-    while( receiveData( Serial1, receive_buffer_slave, slave_index, &slave_reading, nullptr, nullptr ) != TRANS_DATA );
-    new_slave_reading = false;
-    slave_command.command[0] = ACKNOWLEDGE;
-    sendCommand( Serial1, slave_command );
-    sendData( Serial, (byttee *)&slave_reading, sizeof( slave_reading ) );
-    sendData( Serial2, (byte *)&slave_reading, sizeof( slave_reading ) );
+    initStateMachine();
 
+    /*
+     * This delay is in place to give slave time to boot, essentially both Arduinos will
+     * be started at the same time. 
+     */
+    delay( 2000 );
+ 
+    /*
+     * This next part could easilly be its own function, but I didn't think it was worth it.
+     * It will only ever be done once right here, it's a unique operation. 
+     * 
+     * TL;DR: prepareInitialDownlink()
+     */
+     {
+         readings.master.header[0] = '\x01';
+         readings.master.header[1] = '\x21';
+         readings.master.terminator[0] = '\r';
+         readings.master.terminator[1] = '\n';
+         assignEntry( readings.master.time, C_TIME(), sizeof( readings.master.time ) );
+         assignEntry( readings.master.bank, "1", sizeof( readings.master.bank ) );
+         assignEntry( readings.master.so2_reading, "0.00", sizeof( readings.master.so2_reading ) );
+         assignEntry( readings.master.no2_reading, "0.00", sizeof( readings.master.no2_reading ) );
+         assignEntry( readings.master.o3_reading, "0.00", sizeof( readings.master.o3_reading ) );
+         assignEntry( readings.master.temp_reading, "0.00", sizeof( readings.master.temp_reading ) );
+         assignEntry( readings.master.extt_reading, "0.00", sizeof( readings.master.extt_reading ) );
+         assignEntry( readings.master.pressure_reading, "0.00", sizeof( readings.master.pressure_reading ) );
+         assignEntry( readings.master.humidity_reading, "0.00", sizeof( readings.master.humidity_reading ) );
+         assignEntry( readings.master.ext_humidity_reading, "0.00", sizeof( readings.master.ext_humidity_reading ) );
+         assignEntry( readings.master.pump_status, "P: OFF AUTO", sizeof( readings.master.pump_status ) );
+         assignEntry( readings.master.bme_status, statuss.bme_status.c_str(), sizeof( readings.master.bme_status ) );
+         assignEntry( readings.master.am2315_status, statuss.am2315_status.c_str(), sizeof( readings.master.am2315_status ) );
+         assignEntry( readings.master.sd_status, statuss.sd_status.c_str(), sizeof( readings.master.sd_status ) );
+         assignEntry( readings.master.reading_status, "FIRST", sizeof( readings.master.reading_status ) );
+     }
+
+     /*
+      * Downlink the prepared reading to two possible places:
+      * 1.) Downlink to HASP
+      * 2.) Downlink to Bluetooth (if the BT hack is still in place )
+      */
+    Serial.println( "sending ground reading" );
+    sendData( ground_serial, (byte *)&readings.master, sizeof( readings.master ) );
+    sendData( blu_serial, (byte *)&readings.master, sizeof( readings.master ) );
+
+    /*
+     * Once the data has been downlinked...
+     * Wait for Slave to send its initial prepared readings (like Master does)
+     */
+    while( !slave_serial.available() );
+    while( receiveData( slave_serial, buffers.slave, buffers.slave_index ) != TRANS_DATA );
+    
+    /*
+     * I don't want to check the boolean here, if we got anything from slave it must have initialized
+     * and later we will simply throw out corrupt readings and request new ones
+     */
+    if( !bufferToReading( buffers.slave, readings.slave ) )
+        Serial.println( "Returning false" );
+    else
+        Serial.println( "Returning true" );
+
+    Serial.println( "anything...." );
+    sendCommand( slave_serial, ACKNOWLEDGE );
+    Serial.println( "anything...." );
+    sendData( ground_serial, (byte *)&readings.slave, sizeof( SENSOR_READING ) );
+    Serial.println( "anything...." );
+    //sendData( blu_serial, (byte *)&readings.slave, sizeof( readings.slave ) );
+
+    Serial.println( "Exiting the Setup loop" );
 }
 
 void loop()
-{  
-    checkGround();
-    checkSlave();
-    if( ( downlink_schedule + 1000 ) < millis() )
-    {
-        downlinkToHasp();
-        downlink_schedule = millis();
-    }
-    sample();
-
-    pump_logic();
-}
-
-void checkGround( void )
 {
-    TRANS_TYPE transmission;
-    
-    if( !Serial.available() )
-        return;
-
-    //receiveData takes slave_reading here due to some really weird, what I believe to be, Arduino error
-    if( ( transmission = receiveData( Serial, receive_buffer_ground, ground_index, &slave_reading, &master_command, &current_gtp ) ) == TRANS_INCOMPLETE )
-        return;
-
-    switch( transmission )
-    {
-        case TRANS_COMMAND:
-            doCommand();
-            break;
-        case TRANS_GTP:
-            parseGTP();
-            break;
-    }
+    Serial.println( "Current State: " + String( current_state ) );
+    current_state = state_machine[current_state].run();
+    if( current_state == NONE_SPECIFIC )
+        current_state = determineTransition();
 }
-
-void checkSlave( void )
-{
-    TRANS_TYPE transmission;
-
-    if( !Serial1.available() )
-        return;
-             
-    if( ( transmission = receiveData( Serial1, receive_buffer_slave, slave_index, &slave_reading, nullptr, nullptr ) ) != TRANS_DATA )
-        return;
-
-    new_slave_reading = true;
-}
-
-void downlinkToHasp( void )
-{
-    SENSOR_READING *to_send;
-
-    //if we are supposed to send bank 2 but don't have a new reading, skip downlinking
-    if( which_bank == 2 && !new_slave_reading )
-    {
-        if( slave_wait_sanity++ > 10 )
-        {
-            slave_command.command[0] = REQUEST_READING;
-            sendCommand( Serial1, slave_command );
-            slave_wait_sanity = 0;
-        }
-        Serial.println( "Waiting for Slave" );
-        return;
-    }
-    
-    switch( which_bank )
-    {
-        case 1:
-            prepareMasterReading();
-            to_send = &master_reading;
-            break;
-        case 2:
-            to_send = &slave_reading;
-            new_slave_reading = false;
-            break;    
-    }
-
-    //write and send
-    writeSD( to_send );
-    sendData( Serial, (byte *)to_send, sizeof( SENSOR_READING ) );
-    sendData( Serial2, (byte *)to_send, sizeof( SENSOR_READING ) );
-
-    //clean up to get ready for next round
-    which_bank = which_bank == 1 ? 2 : 1;
-    if( which_bank == 2 )
-    {
-        slave_command.command[0] = REQUEST_READING;
-        sendData( Serial1, (byte *)&slave_command, sizeof( slave_command ) );
-        //sendCommand( Serial1, slave_command );
-    }
-}
-
-void sample( void )
-{   
-    sample_set.so2_total += so2.generateReadingPPM();
-    sample_set.so2_count++;
-    sample_set.no2_total += no2.generateReadingPPM();
-    sample_set.no2_count++;
-    sample_set.o3_total += o3.generateReadingPPM();
-    sample_set.o3_count++;
-    sample_set.temp_total += bme.readTemperature();
-    sample_set.temp_count++;
-    sample_set.humidity_total += bme.readHumidity();
-    sample_set.humidity_count++;
-    sample_set.pressure_total += bme.readPressure() / 100.0F;
-    sample_set.pressure_count++;
-    sample_set.ext_temp_total += dongle.readTemperature();
-    sample_set.ext_temp_count++;
-    sample_set.ext_humidity_total += dongle.readHumidity();
-    sample_set.ext_humidity_count++;
-}
-
-void prepareMasterReading( void )
-{
-    double so2_ppm;
-    double no2_ppm;
-    double o3_ppm;
-    double temp;
-    double humidity;
-    double pressure;
-    double ext_temp;
-    double ext_humidity;
-
-    so2_ppm = sample_set.so2_total / sample_set.so2_count;
-    no2_ppm = sample_set.no2_total / sample_set.no2_count;
-    o3_ppm  = sample_set.o3_total / sample_set.o3_count;
-
-    temp = sample_set.temp_total / sample_set.temp_count;
-    humidity = sample_set.humidity_total / sample_set.humidity_count;
-    pressure = sample_set.pressure_total / sample_set.pressure_count;
-
-    ext_temp = sample_set.ext_temp_total / sample_set.ext_temp_count;
-    ext_humidity = sample_set.ext_humidity_total / sample_set.ext_humidity_count;
-
-    memset( &sample_set, 0, sizeof( sample_set ) );
-
-
-    assignEntry( master_reading.time, String( gtp_time +  ( ( millis() - gtp_received_at ) / 1000.0F ) ).c_str(), sizeof( master_reading.time ) );
-    assignEntry( master_reading.bank, "1", sizeof( master_reading.bank ) );
-    assignEntry( master_reading.so2_reading, String( so2_ppm ).c_str(), sizeof( master_reading.so2_reading ) );
-    assignEntry( master_reading.no2_reading, String( no2_ppm ).c_str(), sizeof( master_reading.no2_reading ) );
-    assignEntry( master_reading.o3_reading, String( o3_ppm ).c_str(), sizeof( master_reading.o3_reading ) );
-    if( bme_status != "BIFD" )
-    {
-       assignEntry( master_reading.temp_reading, String( temp ).c_str(), sizeof( master_reading.temp_reading ) );
-       assignEntry( master_reading.pressure_reading, String( pressure ).c_str(), sizeof( master_reading.pressure_reading ) );
-       assignEntry( master_reading.humidity_reading, String( humidity ).c_str(), sizeof( master_reading.humidity_reading ) );
-    }
-    if( am2315_status != "AIFD" )
-    {
-        assignEntry( master_reading.extt_reading, String( ext_temp ).c_str(), sizeof( master_reading.extt_reading ) );
-        assignEntry( master_reading.ext_humidity_reading, String( ext_humidity ).c_str(), sizeof( master_reading.ext_humidity_reading ) );
-    }
-    assignEntry( master_reading.pump_status, pump_on ? "PUMP ON" : "PUMP OFF", sizeof( master_reading.pump_status ) );
-    assignEntry( master_reading.sd_status, sd_status.c_str(), sizeof( master_reading.sd_status ) );
-    assignEntry( master_reading.reading_status, reading_status.c_str(), sizeof( master_reading.reading_status ) );
-}
-
-void writeSD( SENSOR_READING *reading )
-{
-    File goat_log;
-    byte *ptr;
-
-    if( !( goat_log = SD.open( log_name ,FILE_WRITE ) ) )
-    {
-        sd_status = "WRITEFAIL";
-        return;         
-    }
-    
-    ptr = (byte *)&reading->time[0];
-    
-    for( int x = 0; x < sizeof( SENSOR_READING ); x++ )
-    {
-        goat_log.write(*ptr++);
-    }
-
-    goat_log.close();
-    
-}
-
-void doCommand( void )
-{
-    Serial.println( "Command received." );   
-}
-
-void parseGTP( void )
-{
-    Serial.println( "GPS received." );
-    char buf[256];
-    gtp_received_at = millis();
-    sscanf( current_gtp.data, "%f,%s", &gtp_time, buf );
-}
-
