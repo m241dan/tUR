f1f351d4230207526e64b1956cfdc3a049ee015c
Daniel R. Koris <korisd@gmail.com>
2017-07-21 09:26:10 -0700

    added slave funcs to the main file and got rid of goat_slave_funcs/

--- Code/HASP/GOAT_SLAVE/GOAT_SLAVE.ino
+++ Code/HASP/GOAT_SLAVE/GOAT_SLAVE.ino
@@ -34,6 +34,38 @@ typedef struct data_set
 
 DATA_SET sample_set;
 
+void setupSlaveSerials( void )
+{
+   //Serial to Master
+   Serial.begin( 300 );
+   while( !Serial );
+}
+
+void setupSlaveGlobals( void )
+{
+    memset( &slave_reading.time[0], ' ', sizeof( slave_reading ) - 4 );
+    memset( &receive_buffer[0], 0, MAX_BUF );
+    buffer_index = 0;
+
+    assignEntry( slave_reading.time, C_TIME(), sizeof( slave_reading.time ) );
+    assignEntry( slave_reading.bank, "2", sizeof( slave_reading.bank ) );
+    assignEntry( slave_reading.extt_reading, "DNA", sizeof( slave_reading.extt_reading ) );
+    assignEntry( slave_reading.ext_humidity_reading, "DNA", sizeof( slave_reading.ext_humidity_reading ) $
+    assignEntry( slave_reading.pump_status, "DNA", sizeof( slave_reading.pump_status ) );
+    assignEntry( slave_reading.sd_status, "DNA", sizeof( slave_reading.sd_status ) );
+    assignEntry( slave_reading.reading_status, "FIRST", sizeof( slave_reading.reading_status ) );
+}
+
+void setupSlaveSensors( void )
+{
+    if( !bme.begin() )
+       bme_status = "BIFD";
+    else
+       bme_status = "BIGD";
+    assignEntry( slave_reading.temp_reading, bme_status.c_str(), sizeof( slave_reading.temp_reading ) );
+}
+
+
 void setup()
 {
     //Setup and initialize sensors
--- Code/libraries/goat_slave_funcs/goat_slave_funcs.h
+++
@@ -1,3 +0,0 @@
-void setupSlaveSerials( void );
-void setupSlaveGlobals( void );
-void setupSlaveSensors( void );
