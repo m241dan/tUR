bee16ce1ede94eb1a7e77e59e3faa1b161c8a11d
Daniel R. Koris <korisd@gmail.com>
2017-08-04 13:55:55 -0500

    new data

--- Data/HASP TV/combiner.lua
+++ Data/HASP TV/combiner.lua
@@ -2,7 +2,7 @@
 
 local lfs = require( "lfs" )
 local dir_name = arg[1]
-local combined_file_name = arg[2] or "combined.out"
+local combined_file_name = arg[2] or "c.out"
 local file_names = {}
 
 for file_name in lfs.dir( dir_name ) do
@@ -14,11 +14,16 @@ for file_name in lfs.dir( dir_name ) do
     end
 end
 
-local combined_file = io.open( combined_file_name, "a+" )
+local combined_file = io.open( combined_file_name, "w" )
 
 for i, file_name in ipairs( file_names ) do
     local curr_file = io.open( file_name, "r" )
-    combined_file:write( curr_file:read( "*a" ) )
+    for line in curr_file:lines() do
+        line = string.gsub( line, "^(\x01)(\x21)", function( a, b ) return ""; end )
+        combined_file:write( line )
+    end
 end
 
 combined_file:close()
+
+
