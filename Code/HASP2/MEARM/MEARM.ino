05d54b85f54d41373f4b5147d23730f5ec552d84
Daniel R. Koris <korisd@gmail.com>
2017-10-18 20:38:43 -0400

    added the file for HASP2 and the first robotic arm!!! eeeee yi yi

---
+++ Code/HASP2/MEARM/MEARM.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+class 
+Servo x_servo;
+Servo y_servo;
+Servo z_servo;
+Servo claw_servo;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
