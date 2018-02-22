47c700ba44fd760cf2a3f328317a8d8f70871463
Daniel R. Koris <korisd@gmail.com>
2018-02-21 20:10:32 -0500

    removed this version

--- Code/HASP2/AprilTagNode/build/apriltags_ros/apriltags/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/craptop/Desktop/catkinws/src/apriltags_ros/apriltags
-# Build directory: /home/craptop/Desktop/catkinws/build/apriltags_ros/apriltags
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
