9e85493018a0209b015214ecfceb95f7f713a842
Daniel R. Koris <korisd@gmail.com>
2018-02-21 20:13:21 -0500

    added this

---
+++ Code/HASP2/AprilTagNode/build/apriltags_ros/apriltags/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/craptop/Desktop/catkinws/src/apriltags_ros/apriltags
+# Build directory: /home/craptop/Desktop/catkinws/build/apriltags_ros/apriltags
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Code/HASP2/AprilTagNode/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/craptop/Desktop/catkinws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/craptop/Desktop/catkinws/devel;/home/craptop/ws_moveit/devel;/opt/ros/kinetic".split(';') if "/home/craptop/Desktop/catkinws/devel;/home/craptop/ws_moveit/devel;/opt/ros/kinetic" != "" else []
