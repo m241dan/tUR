c61d98fbfe06b07e7afd6b6a5860d8e350417d48
Daniel R. Koris <korisd@gmail.com>
2018-02-23 17:54:01 -0500

    removed ros

--- Code/HASP2/_2018-02-23_OleFoamy_SerialRelay/_2018-02-23_OleFoamy_SerialRelay.ino
+++ Code/HASP2/_2018-02-23_OleFoamy_SerialRelay/_2018-02-23_OleFoamy_SerialRelay.ino
@@ -2,18 +2,28 @@
 //Reads in serial data coming out of vac chamber and passes it over USB to computer.
 // 23 Feb 2018 - Jimmy :(
 
-#include "String.h"
-String incomingString = "aaa";
+//#include "String.h"
+String incomingString;
 
 void setup() {
+  Serial1.begin(9600);
   Serial.begin(9600);
 
 }
 
 void loop() {
   //MEGA-ONLY SYNTAX; change for UNO. -JEA
-  incomingString = Serial1.read();
-  Serial.println(incomingString);
-  delay(500);
-
+  static char buf[256];
+  memset( &buf[0], 0, sizeof( 256 ) );
+  incomingString = "";
+  if( Serial1.available() ){
+    for( int i = 0; i < 256; i++ )
+    {
+        buf[i]= Serial1.read();
+        if( Serial1.available() == 0 )
+            break;
+    }
+    incomingString = String( buf );
+    Serial.print( incomingString );
+  }
 }
--- ROS/build/apriltags_ros/apriltags/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/craptop/Desktop/ROS/src/apriltags_ros/apriltags
-# Build directory: /home/craptop/Desktop/ROS/build/apriltags_ros/apriltags
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
