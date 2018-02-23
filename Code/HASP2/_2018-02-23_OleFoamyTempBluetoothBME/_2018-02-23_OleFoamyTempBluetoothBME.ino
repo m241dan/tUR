ec1090ada3323e53db97ef823e3ec2e5f2fbd3ad
Jimmy <kestrel@gmail.com>
2018-02-23 15:08:02 -0500

    Newer Ol' Foamy test code which support BME and re-does the output slightly. -JEA

---
+++ Code/HASP2/_2018-02-23_OleFoamyTempBluetoothBME/_2018-02-23_OleFoamyTempBluetoothBME.ino
@@ -0,0 +1,40 @@
+#include <DallasTemperature.h>
+#include "Adafruit_Sensor.h"
+#include "Adafruit_BME280.h"
+
+#define THERMO 9
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11 
+#define BME_CS 10
+
+OneWire oneWire( THERMO );
+DallasTemperature sensors( &oneWire );
+Adafruit_BME280 bme; // I2C
+
+void setup()
+{
+    Serial.begin( 9600 );
+    while( !Serial );
+    if (!bme.begin()) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      while (1);
+  }
+}
+
+void loop()
+{
+    sensors.requestTemperatures();
+
+    Serial.print( "Therms: " + String(millis() ) + " ms, " +
+                    String( sensors.getTempCByIndex(0) ) + " C  , " +
+                    String( sensors.getTempCByIndex(1) ) + " C\t ");
+
+    Serial.print("BMETemp = ");
+    Serial.print(bme.readTemperature());
+    Serial.print("C\t");
+    Serial.print("BMEPres = ");
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa\t");
+    delay(1000);
+}
