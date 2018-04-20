62f26f7a5040def2395acd5ce0cdef6413f51c57
Daniel R. Koris <korisd@gmail.com>
2018-04-20 16:09:27 -0400

    added the basics of the arm_controller cpp and h

---
+++ ROSCode/RAM/src/arm_controller/src/arm_controller.cpp
@@ -0,0 +1,26 @@
+//
+// Created by korisd on 4/20/18.
+//
+
+#include "arm_controller.h"
+
+int main()
+{
+    setupPublishers();
+    setupSubscribers();
+
+    ros::spin();
+    return 0;
+}
+
+void setupPublishers( ros::NodeHandle &ros_handle )
+{
+    return;
+}
+
+void setupSubscribers( ros::NodeHandle &ros_handle )
+{
+
+    return;
+}
+
---
+++ ROSCode/RAM/src/arm_controller/src/arm_controller.h
@@ -0,0 +1,35 @@
+//
+// Created by korisd on 4/20/18.
+//
+
+#ifndef RAM_ARM_CONTROLLER_H
+#define RAM_ARM_CONTROLLER_H
+
+#include <ros/ros.h>
+#include <string>
+
+/*
+ * Publishers
+ */
+ros::Publisher queue_size;
+ros::Publisher state_machine_mode;
+ros::Publisher desired_mode;
+ros::Publisher current_kinematics;
+ros::Publisher goal_kinematics;
+ros::Publisher servo_one_info;
+ros::Publisher servo_two_info;
+ros::Publisher servo_three_info;
+ros::Publisher servo_four_info;
+
+/*
+ * Subscribers
+ */
+
+ros::Subscriber enqueue_waypoint;
+ros::Subscriber reset_queue;
+ros::Subscriber operation_mode;
+
+void setupPublishers( ros::NodeHandle &ros_handle  );
+void setupSubscribers( ros::NodeHandle &ros_handle );
+
+#endif //RAM_ARM_CONTROLLER_H
