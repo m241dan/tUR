6bca235de1730e4ce1f5e45b1d837d39b0bf2aac
Daniel R. Koris <korisd@gmail.com>
2018-04-21 12:35:49 -0400

    got the port hanlder into the init process

--- ROSCode/RAM/src/arm_controller/src/arm_controller.cpp
+++ ROSCode/RAM/src/arm_controller/src/arm_controller.cpp
@@ -4,10 +4,18 @@
 
 #include "arm_controller.h"
 
-int main()
+int main( int argc, char **argv )
 {
-    setupPublishers();
-    setupSubscribers();
+    ros::init( argc, argv, "arm_controller", ros::init_options::NoSigintHandler );
+    ros::NodeHandle ros_handle;
+
+    setupPublishers( ros_handle );
+    setupSubscribers( ros_handle );
+
+    /*
+     * turn /dev/ttyUSB0 into a var/macro at some point 
+     */
+    dynamixel::PortHandler *port_handler = dynamixel::PortHandler::getPortHandler( "/dev/ttyUSB0" );
 
     ros::spin();
     return 0;
