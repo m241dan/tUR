3765b7fff985df9e1e68d4f2e564fc174befda01
Daniel R. Koris <korisd@gmail.com>
2018-04-21 15:06:52 -0400

    got publishers and subscribers setup, the callbacks for subs needs programming, but that will come later. Also working on setting up the dynamixel bus

--- ROSCode/RAM/src/arm_controller/src/arm_controller.cpp
+++ ROSCode/RAM/src/arm_controller/src/arm_controller.cpp
@@ -11,11 +11,8 @@ int main( int argc, char **argv )
 
     setupPublishers( ros_handle );
     setupSubscribers( ros_handle );
-
-    /*
-     * turn /dev/ttyUSB0 into a var/macro at some point 
-     */
-    dynamixel::PortHandler *port_handler = dynamixel::PortHandler::getPortHandler( "/dev/ttyUSB0" );
+    setupDynamixelBus();
+    setupDynamixelDriver();
 
     ros::spin();
     return 0;
@@ -23,12 +20,90 @@ int main( int argc, char **argv )
 
 void setupPublishers( ros::NodeHandle &ros_handle )
 {
+    queue_size = ros_handle.advertise<std_msgs::UInt16>( "arm_queue_size", 10 );
+    state_machine_mode = ros_handle.advertise<std_msgs::UInt8>( "state_machine_mode", 10 );
+    desired_mode = ros_handle.advertise<std_msgs::UInt8>( "desired_mode", 10 );
+    current_kinematics = ros_handle.advertise<geometry_msgs::Pose>( "current_kinematics", 10 );
+    goal_kinematics = ros_handle.advertise<geometry_msgs::Pose>( "goal_kinematics", 10 );
+    servo_rotation_info = ros_handle.advertise<dynamixel_workbench_msgs::XH>( "servo/rotation", 10 );
+    servo_shoulder_info = ros_handle.advertise<dynamixel_workbench_msgs::XH>( "servo/shoulder", 10 );
+    servo_elbow_info = ros_handle.advertise<dynamixel_workbench_msgs::XH>( "servo/elbow", 10 );
+    servo_wrist_info = ros_handle.advertise<dynamixel_workbench_msgs::XH>( "servo/wrist", 10 );
     return;
 }
 
 void setupSubscribers( ros::NodeHandle &ros_handle )
 {
-
+    enqueue_waypoint = ros_handle.subscribe( "arm/waypoint", 10, enqueueHandler );
+    reset_queue = ros_handle.subscribe( "arm/queue_reset", 10, resetQueueHandler );
+    operation_mode = ros_handle.subscribe( "arm/mode_setter", 10, operationModeHandler );
     return;
 }
 
+bool setupDynamixelBus()
+{
+    bool success = false;
+    /*
+     * turn /dev/ttyUSB0 into a var/macro at some point
+     */
+    if( bench.begin( "/dev/ttyUSB0" ) )
+    {
+        messaging::successMsg( __FUNCTION__, "Opened dynamixel servo bus successfully." );
+        success = true;
+    }
+    else
+    {
+        messaging::errorMsg( __FUNCTION__, "Failed to open dynamixel servo bus." );
+    }
+    return success;
+}
+
+bool setupDynamixelDriver()
+{
+    uint8_t num_servos;
+    uint8_t servo_ids[5] = { 0, 0, 0, 0, 0 };
+    bool success = false;
+
+    bench.scan( servo_ids, &num_servos, 4 );
+
+    if( num_servos != 4 )
+    {
+        /* check for correct count */
+        messaging::errorMsg( __FUNCTION__, "Incorrect number of servos found on scan. " );
+        success = false;
+    }
+    else
+    {
+        /*
+         * make sure there are no zero ID servos
+         * this is sort of a useless test... but humor me for now
+         */
+        for( int i = 1; i < 5; i++ )
+        {
+            if( servo_ids[i] == 0 )
+            {
+                messaging::errorMsg( __FUNCTION__, "A servo with ID 0 found..." );
+                success = false;
+            }
+        }
+
+    }
+    return success;
+}
+
+/*
+ * Subscriber Callbacks
+ */
+void enqueueHandler( const geometry_msgs::Pose::ConstPtr &message )
+{
+
+}
+
+void resetQueueHandler( const std_msgs::UInt8::ConstPtr &message )
+{
+
+}
+void operationModeHandler( const std_msgs::UInt8::ConstPtr &message )
+{
+    
+}
--- ROSCode/RAM/src/arm_controller/src/arm_controller.h
+++ ROSCode/RAM/src/arm_controller/src/arm_controller.h
@@ -7,7 +7,14 @@
 
 #include <ros/ros.h>
 #include <string>
-#include "dynamixel_sdk/port_handler.h"
+#include <geometry_msgs/Pose.h>
+#include <std_msgs/UInt8.h>
+#include <std_msgs/UInt16.h>
+#include "arm_controller/kinematics.h"
+#include "state_machine/Error.h"
+#include "dynamixel_workbench_toolbox/dynamixel_workbench.h"
+#include "dynamixel_workbench_msgs/XH.h"
+
 
 /*
  * Publishers
@@ -17,10 +24,10 @@ ros::Publisher state_machine_mode;
 ros::Publisher desired_mode;
 ros::Publisher current_kinematics;
 ros::Publisher goal_kinematics;
-ros::Publisher servo_one_info;
-ros::Publisher servo_two_info;
-ros::Publisher servo_three_info;
-ros::Publisher servo_four_info;
+ros::Publisher servo_rotation_info;
+ros::Publisher servo_shoulder_info;
+ros::Publisher servo_elbow_info;
+ros::Publisher servo_wrist_info;
 
 /*
  * Subscribers
@@ -30,7 +37,15 @@ ros::Subscriber enqueue_waypoint;
 ros::Subscriber reset_queue;
 ros::Subscriber operation_mode;
 
+DynamixelWorkbench bench;
+
 void setupPublishers( ros::NodeHandle &ros_handle  );
 void setupSubscribers( ros::NodeHandle &ros_handle );
+bool setupDynamixelBus();
+bool setupDynamixelDriver();
+
+void enqueueHandler( const geometry_msgs::Pose::ConstPtr &message );
+void resetQueueHandler( const std_msgs::UInt8::ConstPtr &message );
+void operationModeHandler( const std_msgs::UInt8::ConstPtr &message );
 
 #endif //RAM_ARM_CONTROLLER_H
