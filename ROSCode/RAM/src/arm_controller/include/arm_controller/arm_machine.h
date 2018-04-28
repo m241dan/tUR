4d19370b07df068807e93fd99e668aba881d46be
Daniel R. Koris <korisd@gmail.com>
2018-04-28 13:08:24 -0400

    working on state machine for arm controller

--- ROSCode/RAM/src/arm_controller/include/arm_controller/arm_machine.h
+++ ROSCode/RAM/src/arm_controller/include/arm_controller/arm_machine.h
@@ -5,6 +5,16 @@
 #ifndef ARM_CONTROLLER_ARM_MACHINE_H
 #define ARM_CONTROLLER_ARM_MACHINE_H
 
+#include "state_machine/StateMachine.h"
+#include "shared_types.h"
 
+class ArmMachine : public StateMachine
+{
+    public:
+        ArmMachine( InputsTable *i ) : inputs(i) {}
+    private:
+        InputsTable *inputs;
+
+};
 
 #endif //ARM_CONTROLLER_ARM_MACHINE_H
