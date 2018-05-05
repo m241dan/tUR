e121022d8a373bcc612f74e0ffcbc4163ca32457
Daniel R. Koris <korisd@gmail.com>
2018-05-05 14:07:48 -0400

    adjust to inherit from GoState and prototyped necessary functions

--- ROSCode/RAM/src/arm_controller/include/arm_controller/GoSynchronizedState.h
+++ ROSCode/RAM/src/arm_controller/include/arm_controller/GoSynchronizedState.h
@@ -5,11 +5,15 @@
 #ifndef ARM_CONTROLLER_GOSYNCHRONIZEDSTATE_H
 #define ARM_CONTROLLER_GOSYNCHRONIZEDSTATE_H
 
-#include "ArmState.h"
+#include "GoState.h"
 
-class GoSynchronizedState : public ArmState
+class GoSynchronizedState : public GoState
 {
-
+    public:
+        GoSynchronizedState( InputsTable *i );
+        virtual std::string transition();
+    protected:
+        void forceTransition( iGoState transition_to );
 
 };
 
