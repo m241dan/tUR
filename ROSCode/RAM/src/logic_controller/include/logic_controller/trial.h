ce839bfc98dd9348b17cea984d7f55bf75ba9f01
Daniel R. Koris <korisd@gmail.com>
2018-05-11 15:31:24 -0400

    working on trial stuff now that action is how I want

--- ROSCode/RAM/src/logic_controller/include/logic_controller/trial.h
+++ ROSCode/RAM/src/logic_controller/include/logic_controller/trial.h
@@ -5,4 +5,23 @@
 #ifndef TRIAL_H
 #define TRIAL_H
 
+#include <geometry_msgs/Pose.h>
+#include <apriltags_ros/AprilTagDetectionArray.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseArray.h>
+
+class Trial
+{
+    public:
+        Trial( std::string trial_name );
+        void setPresentKinematics( geometry_msgs::Pose *pk );
+        void setPresentDetections( std::vector<geometry_msgs::PoseStamped> *pd );
+        std::vector<geometry_msgs::PoseArray> generateWaypoints();
+        bool isComplete();
+    private:
+        geometry_msgs::Pose *present_kinematics;
+        std::vector<geometry_msgs::PoseStamped> *present_detections;
+        //std::vector<Action> action_queue;
+};
+
 #endif //TRIAL_H
--- ROSCode/RAM/src/logic_controller/src/trial.cpp
+++ ROSCode/RAM/src/logic_controller/src/trial.cpp
@@ -2,3 +2,5 @@
 // Created by korisd on 5/11/18.
 //
 
+#include "logic_controller/trial.h"
+
