ab7dd399538a4200990179458382cbbafdcd064f
Daniel R. Koris <korisd@gmail.com>
2018-05-11 15:29:58 -0400

    got actions setup as structs, no need for them to be objects

--- ROSCode/RAM/src/logic_controller/include/logic_controller/action.h
+++ ROSCode/RAM/src/logic_controller/include/logic_controller/action.h
@@ -5,4 +5,23 @@
 #ifndef ACTION_H
 #define ACTION_H
 
+#include <string>
+
+typedef enum
+{
+    VISION, DISCRETE_R, DISCRETE_W, MAX_ACTION
+} ActionType;
+
+typedef struct trial_action
+{
+    std::string name = "default-action";
+    ActionType type = VISION;
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
+    uint8_t precision = 0;
+    std::string shape = "linear";
+    uint8_t tag_id = 0;
+} Action;
+
 #endif //ACTION_H
