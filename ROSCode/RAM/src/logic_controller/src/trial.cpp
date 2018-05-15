184a1020012828afea9137bf2ed9fade5bf066e9
Daniel R. Koris <korisd@gmail.com>
2018-05-15 17:00:08 -0400

    added test trials and debugged

--- ROSCode/RAM/src/logic_controller/src/trial.cpp
+++ ROSCode/RAM/src/logic_controller/src/trial.cpp
@@ -4,23 +4,87 @@
 
 #include "logic_controller/trial.h"
 
-Trial::Trial( std::string trial_name ) : name( trial_name ), action_tracker(0), trial_complete(false)
+Trial::Trial( std::string trial_name ) : name( trial_name ), action_tracker( 0 ), trial_complete( false )
 {
     //load trial with Lua
     //load actions with Lua
     if( name == "discrete_w" )
     {
+        Action action;
+        action.eeo = 0.0;
+        action.x = 18.0;
+        action.y = 12.0;
+        action.z = 10.0;
+        action.velocity = 20;
+        action.type = DISCRETE_W;
+        action.precision = 10;
+        action.shape = "linear";
 
+        action_queue.push_back( action );
+
+        action.y = -12.0;
+
+        action_queue.push_back( action );
+
+        action.z = 20.0;
+
+        action_queue.push_back( action );
+
+        action.y = 12.0;
+
+        action_queue.push_back( action );
     }
     else if( name == "discrete_r" )
     {
+        Action action;
+        action.eeo = 0;
+        action.x = 5.0;
+        action.y = 5.0;
+        action.z = 5.0;
+        action.velocity = 20;
+        action.type = DISCRETE_R;
+        action.precision = 6;
+        action.shape = "linear";
 
+        action_queue.push_back( action );
+
+        action.x = 0.0;
+        action.z = 0.0;
+        action.y = -10.0;
+
+        action_queue.push_back( action );
+
+        action.x = -5.0;
+        action.y = 10.0;
+        action.z = 5.0;
+        action.shape = "parabolic";
+
+        action_queue.push_back( action );
     }
     else
     {
         /* mixed */
+        Action action;
+        action.eeo = 0.0;
+        action.x = 15.0;
+        action.y = 0.0;
+        action.z = 10.0;
+        action.velocity = 20;
+        action.type = DISCRETE_W;
+        action.precision = 5;
+        action.shape = "linear";
+
+        action_queue.push_back( action );
+
+        action.x = -5.0;
+        action.y = -10.0;
+        action.z = 0.0;
+        action.type = DISCRETE_R;
+        action.shape = "parabolic";
+
+        action_queue.push_back( action );
     }
-    present_action = &action_queue.front();
+    action_tracker = 0;
 }
 
 void Trial::setPresentKinematics( geometry_msgs::Pose *pk )
@@ -37,15 +101,19 @@ geometry_msgs::PoseArray Trial::generateWaypoints()
 {
     geometry_msgs::PoseArray waypoints;
 
-    if( action_queue.size() > 0 && present_action )
+    if( action_queue.size() > 0 )
     {
-        switch( present_action->type )
+        present_action = action_queue.at( action_tracker );
+        switch( present_action.type )
         {
             default:
                 break;
             case DISCRETE_R:
             {
                 /*
+                 * THIS DESCRIPTION MAY NOW BE OUT OF DATE
+                 * ALSO NEEDS REFACTORING
+                 *
                  * This essentially is generating points along a space curve that is either linear (t) or parabolic (t^2)
                  * r(t) = < At, Bt, Ct> or r(t) = < At^2, Bt^2, Ct^2 >
                  * To get the constants, we use the final position for the waypoint for the solution to the equation
@@ -59,27 +127,30 @@ geometry_msgs::PoseArray Trial::generateWaypoints()
                  * waypoints, take current position and add each vector to generate the waypoints. The same is also done
                  * with the end effector, so technically there is a "4th" dimension.
                  */
-                std::tuple<double,double,double,double> constants = generateConstants();
+                std::tuple<double, double, double, double> constants = generateConstants( present_action.x,
+                                                                                          present_action.y,
+                                                                                          present_action.z,
+                                                                                          present_action.eeo );
 
                 const double A = std::get<0>( constants );
                 const double B = std::get<1>( constants );
                 const double C = std::get<2>( constants );
                 const double D = std::get<3>( constants );
 
-                for( int i = 1; i <= present_action->precision; i++ )
+                for( int i = 1; i <= present_action.precision; i++ )
                 {
                     double t;
-                    if( present_action->shape == "linear" )
-                        t = (double)i;
+                    if( present_action.shape == "linear" )
+                        t = (double) i;
                     else
-                        t = (double)( i * i );
+                        t = (double) (i * i);
 
                     geometry_msgs::Pose pose = *present_kinematics;
                     pose.position.x += A * t;
                     pose.position.y += B * t;
                     pose.position.z += C * t;
                     pose.orientation.w += D * t;
-                    pose.orientation.z = present_action->velocity;
+                    pose.orientation.z = present_action.velocity;
 
                     waypoints.poses.push_back( pose );
                 }
@@ -87,28 +158,40 @@ geometry_msgs::PoseArray Trial::generateWaypoints()
             }
             case DISCRETE_W:
             {
-                std::tuple<double,double,double,double> constants = generateConstants();
+                std::tuple<double, double, double, double> constants = generateConstants(
+                        (present_action.x - present_kinematics->position.x),
+                        (present_action.y - present_kinematics->position.y),
+                        (present_action.z - present_kinematics->position.z),
+                        (present_action.eeo - present_kinematics->orientation.w));
 
                 const double A = std::get<0>( constants );
                 const double B = std::get<1>( constants );
                 const double C = std::get<2>( constants );
                 const double D = std::get<3>( constants );
 
-                for( int i = 1; i <= present_action->precision; i++ )
+                for( int i = 1; i <= present_action.precision; i++ )
                 {
                     double t;
-                    if( present_action->shape == "linear" )
+                    if( present_action.shape == "linear" )
+                    {
                         t = (double) i;
+                    }
                     else
+                    {
                         t = (double) (i * i);
+                    }
 
-                    geometry_msgs::Pose pose;
-                    pose.position.x = A * t;
-                    pose.position.y = B * t;
-                    pose.position.z = C * t;
-                    pose.orientation.w = D * t;
-                    pose.orientation.z = present_action->velocity;
+                    geometry_msgs::Pose pose = *present_kinematics;
+                    pose.position.x += A * t;
+                    pose.position.y += B * t;
+                    pose.position.z += C * t;
+                    pose.orientation.w += D * t;
+                    pose.orientation.z = present_action.velocity;
 
+                    std::cout << "Pose.x" << pose.position.x << std::endl;
+                    std::cout << "Pose.y" << pose.position.y << std::endl;
+                    std::cout << "Pose.z" << pose.position.z << std::endl;
+                    std::cout << "Pose.w" << pose.orientation.w << std::endl;
                     waypoints.poses.push_back( pose );
                 }
                 break;
@@ -139,37 +222,37 @@ bool Trial::isActionComplete()
 
 void Trial::nextAction()
 {
-    if( ( action_tracker + 1 ) == action_queue.size() ) //remember we have zero indexing on vectors, vector of size 1 will have one thing at index 0, and tracker is the index
+    if((this->action_tracker + 1) ==
+       action_queue.size()) //remember we have zero indexing on vectors, vector of size 1 will have one thing at index 0, and tracker is the index
     {
         trial_complete = true;
     }
     else
     {
-        action_tracker++;
-        present_action = &action_queue.at( action_tracker );
+        this->action_tracker++;
+        present_action = action_queue.at( action_tracker );
     }
 }
 
-std::tuple<double,double,double,double> Trial::generateConstants()
+std::tuple<double, double, double, double>
+Trial::generateConstants( double new_x, double new_y, double new_z, double new_eeo )
 {
     double A = 0.;
     double B = 0.;
     double C = 0.;
     double D = 0.;
 
-    if( present_action )
-    {
-        double t = 0.00001;
-        if( present_action->shape == "linear" )
-            t = (double) present_action->precision;
-        else
-            t = (double) (present_action->precision * present_action->precision);
 
-        A = present_action->x / t;
-        B = present_action->y / t;
-        C = present_action->z / t;
-        D = present_action->eeo / t;
-    }
+    double t = 0.00001;
+    if( present_action.shape == "linear" )
+        t = (double) present_action.precision;
+    else
+        t = (double) (present_action.precision * present_action.precision);
+
+    A = new_x / t;
+    B = new_y / t;
+    C = new_z / t;
+    D = new_eeo / t;
 
     return std::make_tuple( A, B, C, D );
 };
@@ -178,15 +261,19 @@ bool Trial::verifyAction()
 {
     bool verified = false;
 
-    switch( present_action->type )
+    switch( present_action.type )
     {
-        default: break;
+        default:
+            break;
         case DISCRETE_W:
-            if( fabs( present_kinematics->position.x - present_action->x ) < WORLD_ERROR &&
-                fabs( present_kinematics->position.y - present_action->y ) < WORLD_ERROR &&
-                fabs( present_kinematics->position.z - present_action->z ) < WORLD_ERROR &&
-                fabs( present_kinematics->orientation.w - present_action->eeo ) < WORLD_ERROR )
+            if( fabs( present_kinematics->position.x - present_action.x ) < WORLD_ERROR &&
+                fabs( present_kinematics->position.y - present_action.y ) < WORLD_ERROR &&
+                fabs( present_kinematics->position.z - present_action.z ) < WORLD_ERROR &&
+                fabs( present_kinematics->orientation.w - present_action.eeo ) < WORLD_ERROR )
+            {
                 verified = true;
+            }
+
             break;
         case DISCRETE_R:
             verified = true;
