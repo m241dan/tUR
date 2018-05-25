6a1f92ad4055581ad6fca9e69f6cf3e8ac1bd4a0
Daniel R. Koris <korisd@gmail.com>
2018-05-25 16:35:26 -0400

    working on the new interface with our dynamixel ystem

--- ROSCode/RAM/src/arm_motion/include/arm_motion/DynamixelController.h
+++ ROSCode/RAM/src/arm_motion/include/arm_motion/DynamixelController.h
@@ -18,7 +18,7 @@ class DynamixelController
             if( _valid )
             {
                 ROS_INFO( "%s: Successful startup of Dynamixel Bus %s", __FUNCTION__, bus.c_str() );
-                if( verifyServos() )
+                if( verifyServos( MAX_SERVO ) )
                 {
                     ROS_INFO( "%s: Servos verified and online.", __FUNCTION__ );
                 }
@@ -45,7 +45,7 @@ class DynamixelController
             _valid = false;
         }
 
-        bool verifyServos()
+        bool verifyServos( int expected_number )
         {
             bool success = true;
 
@@ -55,9 +55,9 @@ class DynamixelController
                 uint8_t num_servos;
                 uint8_t servo_ids[4] = {0, 0, 0, 0};
 
-                _bench.scan( servo_ids, &num_servos, 4 );
+                _bench.scan( servo_ids, &num_servos, expected_number );
 
-                if( num_servos != 4 )
+                if( num_servos != expected_number )
                 {
                     ROS_INFO( "%s: invalid number of servos found on scan.", __FUNCTION__ );
                     invalidateBus();
@@ -89,36 +89,77 @@ class DynamixelController
 
         void initializeServos()
         {
+            ServoCommand max_vel;
+            ServoCommand pid_i_gain;
+
+            max_vel.command = "Velocity_Limit";
+            max_vel.value = MAX_VELOCITY;
+            pid_i_gain.command = "Position_I_Gain";
+            pid_i_gain.value = PID_I_GAIN;
+
             for( int i = ROTATION_SERVO; i < MAX_SERVO; i++ )
             {
-              //  _bench.itemWrite();
+                int id = i + 1;
+                max_vel.id = id;
+                pid_i_gain.id = id;
+
+                benchWrite( max_vel );
+                benchWrite( pid_i_gain );
             }
         }
 
-        bool benchWrite( )
+        bool benchWrite( ServoCommand com )
         {
+            bool success = false;
 
+            if( validCommand( com ) )
+                success = _bench.itemWrite( com.id, com.command.c_str(), com.value );
+
+            if( !success )
+                ROS_INFO( "%s: failed to write command { %d, %s, %d }", __FUNCTION__, com.id, com.command.c_str(), com.value );
+
+            return success;
         }
 
         bool validCommand( ServoCommand com )
         {
-            bool valid = true;
+            bool valid = false;
 
-            if( com.id < ROTATION_SERVO || com.id >= MAX_SERVO )
+            /*
+             * Assume the command is bad.
+             * If it is in the valid ID range and command is not ""
+             * Then, check the command field to make sure its valid
+             * If the command shows up in the valid_commands table, it's good.
+             */
+            if( com.id < ROTATION_SERVO || com.id > MAX_SERVO )
             {
-                valid = false;
+                ROS_INFO( "%s: ID[%d] is outside acceptable ranges.", __FUNCTION__, com.id );
             }
             else if( com.command == "" )
             {
-                valid = false;
+                ROS_INFO( "%s: Attempting to send a \"\" command ", __FUNCTION__ );
             }
             else
             {
-
+                for( int i = 0; i < MAX_COMMAND; i++ )
+                {
+                    if( com.command == valid_commands[i] )
+                    {
+                        valid = true;
+                        break;
+                    }
+                }
+                if( !valid )
+                    ROS_INFO( "%s: Command \"%s\" is invalid for X Series.", __FUNCTION__, com.command.c_str() );
             }
 
             return valid;
         }
+        int32_t benchRead( uint8_t id, std::string command )
+        {
+            return _bench.itemRead( id, command.c_str() );
+        }
+
         /*
          * variables
          */
