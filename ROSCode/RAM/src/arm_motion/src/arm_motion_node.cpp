d61543ed9b879dac982ee8ac388842b6949a099f
Daniel R. Koris <korisd@gmail.com>
2018-05-26 14:48:16 -0400

    implemented dynamixel controller

--- ROSCode/RAM/src/arm_motion/src/arm_motion_node.cpp
+++ ROSCode/RAM/src/arm_motion/src/arm_motion_node.cpp
@@ -2,11 +2,18 @@
 // Created by korisd on 5/25/18.
 //
 
+#include <arm_motion/DynamixelController.h>
 #include "arm_motion/arm_motion_node.h"
 
 int main( int argc, char **argv )
 {
+    std::string bus = "/dev/ttyUSB0";
+    ros::init( argc, argv, "arm_motion" );
+    ros::NodeHandle node_handle;
 
+    node_handle.param( "bus_name", bus );
+    DynamixelController controller( bus );
 
+    ros::spin();
     return 0;
 }
