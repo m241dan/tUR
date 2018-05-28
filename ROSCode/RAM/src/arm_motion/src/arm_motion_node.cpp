3f85ac0ae67b447538e2818804d5e4807a2093a4
Daniel R. Koris <korisd@gmail.com>
2018-05-28 12:19:56 -0400

    action server has to be the same name as the node

--- ROSCode/RAM/src/arm_motion/src/arm_motion_node.cpp
+++ ROSCode/RAM/src/arm_motion/src/arm_motion_node.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <arm_motion/DynamixelController.h>
+#include <arm_motion/MotionActor.h>
 #include "arm_motion/arm_motion_node.h"
 
 int main( int argc, char **argv )
@@ -13,7 +14,7 @@ int main( int argc, char **argv )
 
     node_handle.param( "bus_name", bus );
     DynamixelController controller( bus );
-
+    MotionActor actor( ros::this_node::getName(), controller );
     ros::spin();
     return 0;
 }
