a2d7cf85f5370b92882ad75f431cad62908b8798
Daniel R. Koris <korisd@gmail.com>
2018-05-29 13:16:53 -0400

    moved some includes around

--- ROSCode/RAM/src/arm_motion/include/arm_motion/arm_motion_node.h
+++ ROSCode/RAM/src/arm_motion/include/arm_motion/arm_motion_node.h
@@ -8,10 +8,9 @@
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 #include <geometry_msgs/Pose.h>
-#include "arm_motion/ArmMotionAction.h"
-#include <dynamixel_workbench_msgs/XH.h>
 #include <apriltags_ros/AprilTagDetectionArray.h>
-
+#include <std_msgs/UInt8.h>
+#include <dynamixel_workbench_msgs/XH.h>
 
 /*
  * Servo Info
