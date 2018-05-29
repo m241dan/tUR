d2fa5f89d3dcfb912d4c87803701d17f029ae0de
Daniel R. Koris <korisd@gmail.com>
2018-05-29 13:15:50 -0400

    started filling out ArmTrial

--- ROSCode/RAM/src/arm_motion/src/ArmTrial.cpp
+++ ROSCode/RAM/src/arm_motion/src/ArmTrial.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "arm_motion/ArmTrial.h"
+
+ArmTrial::ArmTrial( std::string trial_name, lua_State *lua, bool &success ) : _trial_name(trial_name)
+{
+
+}
+
+void ArmTrial::setupTimers()
+{
+    _run_timer = _node_handle.createTimer( ros::Duration( 0.1 ), boost::bind( &ArmTrial::trialOperation, this, _1 ), false, false );
+}
+
+inline bool ArmTrial::isActive()
+{
+    return _active;
+}
+
+inline bool ArmTrial::isComplete()
+{
+    return _complete;
+}
+
+bool ArmTrial::start()
+{
+    bool success = true;
+    if( !_complete )
+    {
+        _run_timer.start();
+    }
+    else
+    {
+        ROS_ERROR( "%s: %s attempting to start an already completed trial", __FUNCTION__, _trial_name );
+        success = false;
+    }
+    return success;
+}
