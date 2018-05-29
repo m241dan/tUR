3ef284861cc1282523073d8a1a30899fdda77e38
Daniel R. Koris <korisd@gmail.com>
2018-05-28 20:31:44 -0400

    renamed arm_motion_node.cpp

--- ROSCode/RAM/src/arm_motion/src/arm_motion_driver.cpp
+++ ROSCode/RAM/src/arm_motion/src/arm_motion_driver.cpp
@@ -9,7 +9,7 @@
 int main( int argc, char **argv )
 {
     std::string bus = "/dev/ttyUSB0";
-    ros::init( argc, argv, "arm_motion" );
+    ros::init( argc, argv, "arm_driver" );
     ros::NodeHandle node_handle;
 
     node_handle.param( "bus_name", bus );
