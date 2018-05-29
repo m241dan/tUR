fa265a68889beb447a12f98476f1714f2e141ea6
Daniel R. Koris <korisd@gmail.com>
2018-05-29 13:17:08 -0400

    added TrialManager beginnings

---
+++ ROSCode/RAM/src/arm_motion/include/arm_motion/TrialManager.h
@@ -0,0 +1,48 @@
+//
+// Created by korisd on 5/29/18.
+//
+
+#ifndef ARM_MOTION_TRIALMANAGER_H
+#define ARM_MOTION_TRIALMANAGER_H
+
+#include "arm_motion/arm_motion_node.h"
+#include "arm_motion/ArmTrial.h"
+#include <vector>
+
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
+
+class TrialManager
+{
+    public:
+        TrialManager( std::string name );
+    protected:
+        /*
+         * Functions
+         */
+        void setupSubscribers();
+        void setupPublishers();
+        void setupTimers();
+        void setupLua();
+
+        void enqueueTrial( const std_msgs::UInt8ConstPtr &msg );
+        void trialMonitor( const ros::TimerEvent &event );
+        /*
+         * Variables
+         */
+        /* Ros Specific */
+        ros::NodeHandle _node_handle;
+        ros::Subscriber trial_selector;
+
+        ros::Timer _trial_monitor;
+
+        /* Lua Specific */
+        lua_State *_lua_handle;
+        std::vector<ArmTrial> _trial_queue;
+};
+
+
+#endif //ARM_MOTION_TRIALMANAGER_H
