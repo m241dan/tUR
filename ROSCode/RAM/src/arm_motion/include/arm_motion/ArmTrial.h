df29f67f00b5f07c2badc97bc9df6d092ebde9f3
Daniel R. Koris <korisd@gmail.com>
2018-05-29 13:16:59 -0400

    added Lua

--- ROSCode/RAM/src/arm_motion/include/arm_motion/ArmTrial.h
+++ ROSCode/RAM/src/arm_motion/include/arm_motion/ArmTrial.h
@@ -5,10 +5,37 @@
 #ifndef ARM_MOTION_ARMTRIAL_H
 #define ARM_MOTION_ARMTRIAL_H
 
+#include "arm_motion/arm_motion_node.h"
+
+extern "C" {
+    #include "lua.h"
+    #include "lauxlib.h"
+    #include "lualib.h"
+}
 
 class ArmTrial
 {
+    public:
+        ArmTrial( std::string trial_name, lua_State *lua, bool &success );
+        bool isActive();
+        bool isComplete();
+        bool start();
+    protected:
+        /*
+         * Functions
+         */
+        void trialOperation( const ros::TimerEvent &event );
+        void setupTimers();
+        /*
+         * Variables
+         */
+        /* ROS Specific */
+        ros::NodeHandle _node_handle;
+        ros::Timer _run_timer;
 
+        std::string _trial_name;
+        bool _active;
+        bool _complete;
 };
 
 
