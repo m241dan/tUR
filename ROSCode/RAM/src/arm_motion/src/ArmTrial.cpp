f7593e745b9b7d6d1f41d6e3f8ec47798582d8d1
Daniel R. Koris <korisd@gmail.com>
2018-05-30 12:08:31 -0400

    added some new parameters for trials, working on generating the motion inside armtrial

--- ROSCode/RAM/src/arm_motion/src/ArmTrial.cpp
+++ ROSCode/RAM/src/arm_motion/src/ArmTrial.cpp
@@ -62,6 +62,22 @@ ArmTrial::ArmTrial( std::string trial_name, lua_State *lua, bool *success ) : _t
             lua_pop( lua, 1 );
 
             /*
+             * Smoothness
+             */
+            lua_pushstring( lua, "smoothness" );
+            lua_gettable( lua, -2 );
+            action.smoothness = (uint16_t)lua_tointeger( lua, -1 );
+            lua_pop( lua, 1 );
+
+            /*
+             * Tolerance
+             */
+            lua_pushstring( lua, "tolerance" );
+            lua_gettable( lua, -2 );
+            action.tolerance = (uint16_t)lua_tointeger( lua, - 1 );
+            lua_pop( lua, 1 );
+
+            /*
              * Shape
              */
             lua_pushstring( lua, "shape" );
@@ -105,9 +121,9 @@ ArmTrial::ArmTrial( std::string trial_name, lua_State *lua, bool *success ) : _t
         }
         lua_pop( lua, 1 );
         /* stack: nil */
-        *success = true;
         setupSubscribers();
         setupTimers();
+        *success = _action_client.waitForServer( ros::Duration( 2 ) );
     }
 }
 ArmTrial::~ArmTrial()
@@ -150,6 +166,7 @@ bool ArmTrial::start()
     if( !_complete )
     {
         _run_timer.start();
+        generateMotion();
         _active = true;
     }
     else
@@ -158,4 +175,86 @@ bool ArmTrial::start()
         success = false;
     }
     return success;
+}
+
+void ArmTrial::generateMotion()
+{
+    Action &action = _actions.front();
+
+    /*
+     * TODO: currently this is all based on server kinematics only, eventually will be some
+     * TODO: approximation of both vision and servo kinematics
+     */
+    /* figure out our final goal based on type of action */
+    geometry_msgs::Pose motion_goal = generateMotionGoalPose( action );
+    /*
+     * generate constants that will be used to determine goal points along the path of the motion
+     */
+    std::tuple<double,double,double,double> constants = generateConstants( motion_goal, action.precision );
+    const double A = std::get<0>( constants );
+    const double B = std::get<1>( constants );
+    const double C = std::get<2>( constants );
+    const double D = std::get<3>( constants );
+
+    /* generate the goal points along the path of the motion */
+    std::vector<geometry_msgs::Pose> motion_steps;
+    for( int i = 1; i <= action.precision; i++ )
+    {
+        double t = (double)i;
+        geometry_msgs::Pose motion_pose;
+        motion_pose.position.x = A * t;
+        motion_pose.position.y = B * t;
+        motion_pose.position.z = C * t;
+        motion_pose.orientation.w = D * t;
+        motion_steps.push_back( motion_pose );
+    }
+
+    /* buid the joint_goals to send to the action server based on our goal_points */
+    std::vector<sensor_msgs::JointState> joint_goals;
+
+
+}
+
+geometry_msgs::Pose ArmTrial::generateMotionGoalPose( Action a )
+{
+    geometry_msgs::Pose pose;
+
+    pose.orientation.z = a.velocity;
+    switch( a.type )
+    {
+        default: ROS_ERROR( "%s: action contains bad type", __FUNCTION__ ); break;
+        case VISION:break;
+        case DISCRETE_R:
+        {
+            pose.position.x = _servo_based_fk_pose.position.x + a.x;
+            pose.position.y = _servo_based_fk_pose.position.y + a.y;
+            pose.position.z = _servo_based_fk_pose.position.z + a.z;
+            pose.orientation.w = _servo_based_fk_pose.orientation.w + a.eeo;
+            break;
+        }
+        case DISCRETE_W:
+            pose.position.x = a.x;
+            pose.position.y = a.y;
+            pose.position.z = a.z;
+            pose.orientation.w = a.eeo;
+            break;
+    }
+
+    return pose;
+}
+
+std::tuple<double,double,double,double> ArmTrial::generateConstants( geometry_msgs::Pose pose, uint8_t precision )
+{
+    double A = 0.;
+    double B = 0.;
+    double C = 0.;
+    double D = 0.;
+    double t = (double) precision;
+
+    A = pose.position.x / t;
+    B = pose.position.y / t;
+    C = pose.position.z / t;
+    D = pose.orientation.w / t;
+
+    return std::make_tuple( A, B, C, D );
 }
