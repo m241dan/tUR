74fc92021e950dd349a12f302e2fa2270835f048
Daniel R. Koris <korisd@gmail.com>
2018-06-05 20:48:00 -0400

    working on adding a path object

---
+++ ROSCode/RAM/src/arm_motion/include/arm_motion/Path.h
@@ -0,0 +1,19 @@
+//
+// Created by korisd on 6/5/18.
+//
+
+#ifndef ARM_MOTION_PATH_H
+#define ARM_MOTION_PATH_H
+
+#include "arm_motion/arm_motion_node.h"
+#include <geometry_msgs/PoseArray.h>
+
+class Path
+{
+    public:
+        Path( geometry_msgs::Pose )
+    protected:
+};
+
+
+#endif //ARM_MOTION_PATH_H
---
+++ ROSCode/RAM/src/arm_motion/src/Path.cpp
@@ -0,0 +1,5 @@
+//
+// Created by korisd on 6/5/18.
+//
+
+#include "arm_motion/Path.h"
