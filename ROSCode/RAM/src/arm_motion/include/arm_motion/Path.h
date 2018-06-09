d1fc9795f207a639c43a44f41e8cd024cf8f7a12
Daniel R. Koris <korisd@gmail.com>
2018-06-08 21:04:39 -0400

    rev one of Path in

--- ROSCode/RAM/src/arm_motion/include/arm_motion/Path.h
+++ ROSCode/RAM/src/arm_motion/include/arm_motion/Path.h
@@ -7,13 +7,28 @@
 
 #include "arm_motion/arm_motion_node.h"
 #include <geometry_msgs/PoseArray.h>
-#include <arm_motion/action.h>
+#include <arm_motion/MotionMsg.h>
+#include <sensor_msgs/JointState.h>
+#include <tuple>
 
 class Path
 {
     public:
-        Path();
+        Path( arm_motion::MotionMsg motion, geometry_msgs::Pose pose );
     protected:
+        void generateFinalPose();
+        void generatePathConstants();
+        void generatePath();
+        void pathToJointPositions();            //performs the IK
+        void setJointPositionVelocities();
+        void setJointPositionEffort();
+
+        arm_motion::MotionMsg _motion_guidelines;
+        geometry_msgs::Pose _present_pose;
+        geometry_msgs::Pose _final_pose;
+        double _A, _B, _C, _D;
+        geometry_msgs::PoseArray _motion_path;
+        std::vector<sensor_msgs::JointState> _output_joint_states;
 };
 
 
