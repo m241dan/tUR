d94ca185af67d14bc82aad07bb73afd7b705376e
Jimmy <kestrel@gmail.com>
2018-06-10 02:06:31 -0400

    Setting up data packets per the ***v3*** PSIP Sheets.

---
+++ Code/HASP2/ram_funcs/ram_funcs.ino
@@ -0,0 +1,11 @@
+#include <ram_funcs.h>
+//Just a blank sketch to test my header out. -JA
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
