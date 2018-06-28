a93efbc38efe4e43a00b025f0aa81989b6190812
Jimmy <kestrel@gmail.com>
2018-06-27 20:22:49 -0400

    Submitting a known-good version before I start packing busybox data up and probably breaking shit. -JA

--- Code/HASP2/2018-05-10_BusyBoxPrototype/2018-05-10_BusyBoxPrototype.ino
+++ Code/HASP2/2018-05-10_BusyBoxPrototype/2018-05-10_BusyBoxPrototype.ino
@@ -13,32 +13,36 @@ int state_rocker_horiz  = 0;
 int state_rocker_verti  = 0;
 int state_toggle_horiz  = 0;
 int state_toggle_verti  = 0;
+int state_pot_lever     = 0;
+float potentiometer_truncator_hack = 10.23;
 
-void setup() {
+void setup() 
+{
   //initialize switches as inputs
-  pinMode(BUTTON_BLU, INPUT_PULLUP);
-  pinMode(ROCKER_HORIZ, INPUT_PULLUP);
-  pinMode(ROCKER_VERTI, INPUT_PULLUP);
-  pinMode(TOGGLE_HORIZ, INPUT_PULLUP);
-  pinMode(TOGGLE_VERTI, INPUT_PULLUP);
-  pinMode(POTENTIOM_LEVER, INPUT_PULLUP);
+  pinMode(BUTTON_BLU,     INPUT_PULLUP);
+  pinMode(ROCKER_HORIZ,   INPUT_PULLUP);
+  pinMode(ROCKER_VERTI,   INPUT_PULLUP);
+  pinMode(TOGGLE_HORIZ,   INPUT_PULLUP);
+  pinMode(TOGGLE_VERTI,   INPUT_PULLUP);
+  pinMode(POTENTIOM_LEVER,INPUT_PULLUP);
   pinMode(POTENTIOM_KNOB, INPUT_PULLUP);
 
   //Initialize serial port for potentiometer
   Serial.begin(9600);
 }
 
-void loop() {
+void loop() 
+{
   //Read switch states
   state_button_blu    = digitalRead(BUTTON_BLU);
   state_rocker_horiz  = digitalRead(ROCKER_HORIZ);
   state_rocker_verti  = digitalRead(ROCKER_VERTI);
   state_toggle_horiz  = digitalRead(TOGGLE_HORIZ);
   state_toggle_verti  = digitalRead(TOGGLE_VERTI);
+  state_pot_lever     = analogRead(POTENTIOM_LEVER);
 
-
-  Serial.print("BBox Status: ");
-  Serial.print("Butan: \t");
+  Serial.print("BBox ");
+  Serial.print("Btn: \t");
   if (state_button_blu == HIGH) 
   {
     Serial.print("ON\t");
@@ -84,7 +88,13 @@ void loop() {
   {
     Serial.print("--\t");
   }
-  //Clean up the end of the line and wait a bit.
-  Serial.println(" ");
-  delay(200);
+  /*Hack the 0-1023 range of the potentiometer down to 0-100 to reduce data 
+  and clean out noise. 100% my idea. I'm so smart. -JA*/
+  state_pot_lever = state_pot_lever / potentiometer_truncator_hack;
+  Serial.print("Pot: ");
+  Serial.print((int)state_pot_lever);
+  
+  /*Clean up the end of the line and wait a bit.*/
+  Serial.println("");
+  delay(500);
 }
