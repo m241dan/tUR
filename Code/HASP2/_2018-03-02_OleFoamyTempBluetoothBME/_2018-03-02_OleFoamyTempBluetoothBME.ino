6d6c1555871d1d38dfc15839e92ada5f29966e0c
Jimmy <kestrel@gmail.com>
2018-07-04 21:33:35 -0400

    Quick hacktest to check 2 BMEs & new harness for LaRC test.

--- Code/HASP2/_2018-03-02_OleFoamyTempBluetoothBME/_2018-03-02_OleFoamyTempBluetoothBME.ino
+++ Code/HASP2/_2018-03-02_OleFoamyTempBluetoothBME/_2018-03-02_OleFoamyTempBluetoothBME.ino
@@ -1,42 +1,58 @@
-#include <DallasTemperature.h>
+//#include <DallasTemperature.h>
 #include "Adafruit_Sensor.h"
 #include "Adafruit_BME280.h"
 
 #define THERMO 9
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11 
-#define BME_CS 10
+#define BME_SCK 24
+#define BME_MOSI 23
+#define BME_MISO 22 
+#define BME_CS_01 1
+#define BME_CS_00 0
 
-OneWire oneWire( THERMO );
-DallasTemperature sensors( &oneWire );
-Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+//OneWire oneWire( THERMO );
+//DallasTemperature sensors( &oneWire );
+Adafruit_BME280 bme00(BME_CS_00, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+Adafruit_BME280 bme01(BME_CS_01, BME_MOSI, BME_MISO, BME_SCK); // software SPI
 
 void setup()
 {
     Serial.begin( 9600 );
     while( !Serial );
-    if (!bme.begin()) 
+    if ( (!bme00.begin()) || (!bme01.begin()))
     {
-      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      Serial.println("One of your BMEs is dedAF; check wiring!");
+      if ( (!bme00.begin()) && (!bme01.begin()))
+      {
+        Serial.println("Could not find any valid BME280 sensor, check wiring!");
+      }
     }
 }
 
 void loop()
 {
-    sensors.requestTemperatures();
+/*    sensors.requestTemperatures();
     int secs = millis() / 1000; //Oh my God I'm such a kludge. -JA
     Serial.print( String(secs) + " s, \t" +
                     String( sensors.getTempCByIndex(0) ) + " C\t " +
                     String( sensors.getTempCByIndex(1) ) + " C\t " +
                     String( sensors.getTempCByIndex(2) ) + " C\t " +
                     String( sensors.getTempCByIndex(3) ) + " C\t ");
+*/
+    Serial.print("BME00 = ");
+    Serial.print(bme00.readTemperature());
+    Serial.print("C,\t");
+    Serial.print(bme00.readPressure() / 100.0F);
+    Serial.print(" hPa\t");
+    Serial.print(bme00.readHumidity());
+    Serial.print("%,\t");
 
-    Serial.print("BMETemp = ");
-    Serial.print(bme.readTemperature());
-    Serial.print("C\t");
-    Serial.print("BMEPres = ");
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa\t");
+    Serial.print("BME01 = ");
+    Serial.print(bme01.readTemperature());
+    Serial.print("C,\t");
+    Serial.print(bme01.readPressure() / 100.0F);
+    Serial.print(" hPa\t");
+    Serial.print(bme00.readHumidity());
+    Serial.println("%,\t");
+    
     delay(1000);
 }
