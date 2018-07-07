d0ed739cfff59f96bc268fd80823e91c32cfd9d2
Daniel R. Koris <korisd@gmail.com>
2018-07-07 17:17:29 -0400

    added rev 1 of cam monitor

---
+++ ROSCode/RAM/src/cam_monitor/include/cam_monitor/CamMonitor.h
@@ -0,0 +1,33 @@
+//
+// Created by korisd on 7/7/18.
+//
+
+#ifndef CAMMONITOR_H
+#define CAMMONITOR_H
+
+#include <cam_monitor/monitor.h>
+
+class CamMonitor
+{
+    public:
+        CamMonitor();
+    protected:
+        ros::NodeHandle         _node_handle;
+        ros::Subscriber         _image_sub;
+        ros::Subscriber         _time_sub;
+        ros::ServiceServer      _take_snap;
+        rosgraph_msgs::Clock    _clock;
+
+        cv_bridge::CvImagePtr   _recent_img;
+        std::string             _img_topic;
+        std::string             _video_location;
+        std::string             _snap_location;
+
+        void timeCallback( const rosgraph_msgs::Clock::ConstPtr &msg );
+        void imageCallback( const sensor_msgs::Image::ConstPtr &msg );
+        bool serviceCallback( cam_monitor::SnapRequest &request, cam_monitor::SnapResponse &response );
+
+};
+
+
+#endif
---
+++ ROSCode/RAM/src/cam_monitor/src/CamMonitor.cpp
@@ -0,0 +1,42 @@
+//
+// Created by korisd on 7/7/18.
+//
+
+#include "cam_monitor/CamMonitor.h"
+
+CamMonitor::CamMonitor()
+{
+    _node_handle.param( "img_topic", _img_topic, std::string( "/usb_cam_node_one/image_raw" ) );
+    _node_handle.param( "video_location", _video_location, std::string( "~/Videos/" ) );
+    _node_handle.param( "snap_location", _snap_location, std::string( "~/Pictures/" ) );
+    _image_sub  = _node_handle.subscribe( _img_topic, 10, &CamMonitor::imageCallback, this );
+    _time_sub   = _node_handle.subscribe( "/clock", 10, &CamMonitor::timeCallback, this );
+    _take_snap  = _node_handle.advertiseService( ( ros::this_node::getName() + "/takeSnap" ), &CamMonitor::serviceCallback, this );
+}
+
+void CamMonitor::timeCallback( const rosgraph_msgs::Clock::ConstPtr &msg )
+{
+    _clock = *msg;
+}
+
+void CamMonitor::imageCallback( const sensor_msgs::Image::ConstPtr &msg )
+{
+    _recent_img = cv_bridge::toCvCopy( msg, sensor_msgs::image_encodings::BGR8 );
+
+    if( !boost::filesystem::exists( _video_location) )
+        boost::filesystem::create_directories( _video_location );
+
+    std::stringstream img_name;
+    img_name << _video_location << _clock.clock.toSec() << ".jpg";
+    cv::imwrite( img_name.str(), _recent_img->image );
+}
+
+bool CamMonitor::serviceCallback( cam_monitor::SnapRequest &request, cam_monitor::SnapResponse &response )
+{
+    if( !boost::filesystem::exists( _snap_location ) )
+        boost::filesystem::create_directories( _snap_location );
+
+    cv::imwrite( ( _snap_location + "snap.jpg" ), _recent_img->image );
+    response.done = true;
+    return true;
+}
