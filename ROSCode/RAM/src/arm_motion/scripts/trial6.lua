d45aea91bd38be7224c5c77ffb2ec9d3794ee116
Daniel R. Koris <korisd@gmail.com>
2018-07-26 02:48:37 -0400

    attempting to fine tune trials

--- ROSCode/RAM/src/arm_motion/scripts/trial6.lua
+++ ROSCode/RAM/src/arm_motion/scripts/trial6.lua
@@ -11,7 +11,7 @@ require('types')
 --trial 6 "Blue Button from Home"
 return {
     [1] = { velocity = 5, type = SERVO_ABSOLUTE, s1 = 0, s2 = 0, s3 = 0, s4 = 0, s5 = 0, s6 = GRIPPER_CLOSED},
-    [2] = { x = 22.00, y = -5.00, z = 20.5, eeo = 0, velocity = 5, type = DISCRETE_W, precision = 1, shape = "linear", smoothness = 20, tolerance = 2},
-    [3] = { x = 23.4, y = -5.1, z = 20.6, eeo = 0, velocity = 5, type = DISCRETE_W, precision = 1, shape = "linear", smoothness = 20, tolerance = 2 },
+    [2] = { x = 22.00, y = -5.00, z = 20.5, eeo = 0, velocity = 5, type = DISCRETE_W, precision = 1, shape = "linear", smoothness = 2, tolerance = 2},
+    [3] = { x = 23.4, y = -5.1, z = 20.6, eeo = 0, velocity = 5, type = DISCRETE_W, precision = 1, shape = "linear", smoothness = 2, tolerance = 2 },
     [4] = { x = 22.00, y =-5.00, z = 20.5, eeo = 0, velocity = 5, type = DISCRETE_W, precision = 1 }
 }
