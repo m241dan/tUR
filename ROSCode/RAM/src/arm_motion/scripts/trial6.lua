c08583317815efbdd31db564c95edf099472290b
Daniel R. Koris <korisd@gmail.com>
2018-07-26 09:49:33 -0400

    trials update

--- ROSCode/RAM/src/arm_motion/scripts/trial6.lua
+++ ROSCode/RAM/src/arm_motion/scripts/trial6.lua
@@ -13,5 +13,5 @@ return {
     [1] = { velocity = 5, type = SERVO_ABSOLUTE, s1 = 0, s2 = 0, s3 = 0, s4 = 0, s5 = 0, s6 = GRIPPER_CLOSED},
     [2] = { x = 22.00, y = -5.00, z = 20.5, eeo = 0, velocity = 5, type = DISCRETE_W, precision = 1, shape = "linear", smoothness = 2, tolerance = 2},
     [3] = { x = 23.4, y = -5.1, z = 20.6, eeo = 0, velocity = 5, type = DISCRETE_W, precision = 1, shape = "linear", smoothness = 2, tolerance = 2 },
-    [4] = { x = 22.00, y =-5.00, z = 20.5, eeo = 0, velocity = 5, type = DISCRETE_W, precision = 1 }
+    [4] = { x = 22.00, y =-5.00, z = 20.5, eeo = 0, velocity = 5, type = DISCRETE_W, precision = 1, shape = "linear", smoothness = 2, tolerance = 2 },
 }
