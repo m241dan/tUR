1c81e2cfc57851a8df92db50170deff4546cd0f8
Daniel R. Koris <korisd@gmail.com>
2018-07-27 14:02:10 -0400

    got the start in

--- ROSCode/RAM/src/ram_synchronizer/src/synchronizer.cpp
+++ ROSCode/RAM/src/ram_synchronizer/src/synchronizer.cpp
@@ -2,4 +2,19 @@
 // Created by korisd on 7/27/18.
 //
 
+#include <ros/ros.h>
+#include <std_srvs/Trigger.h>
+
+int main( int argc, char *argv[] )
+{
+    // setup services for calling the processes that are synchtonized
+    // setup serial loop counter for scaling the 5 Hz to 1 in the same loop
+    // while loop
+    // rate 5 Hz
+        // filter serial read/writes at 1 Hz (so once every 5 times)
+            // service call to do serial loop
+        // service call to do servo loop
+        // service call to do i2c loop
+    return 0;
+}
 
