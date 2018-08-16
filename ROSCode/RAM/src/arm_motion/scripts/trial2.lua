b771621fe34f3d91683e786dca0ee56119130002
Daniel R. Koris <korisd@gmail.com>
2018-08-16 12:48:38 -0400

    adding a little more wiggle room to the go home trial

--- ROSCode/RAM/src/arm_motion/scripts/trial2.lua
+++ ROSCode/RAM/src/arm_motion/scripts/trial2.lua
@@ -12,7 +12,7 @@ require('types')
 -- Ending position X: 20.42, Y: 14.33, Z: 5.73
 
 return {
-    [1] =  { x =  18.95, y =  12.96, z = 13.19, eeo = 0.292, velocity = 5, type = DISCRETE_W, precision = 1, shape = "linear", smoothness=10, tolerance=2 };
+    [1] =  { x =  18.95, y =  13.0, z = 13.19, eeo = 0.292, velocity = 5, type = DISCRETE_W, precision = 1, shape = "linear", smoothness=10, tolerance=2 };
      [2] = { velocity = 5, type = SERVO_ABSOLUTE, s1 = 0, s2 = -190, s3 = 0, s4 = 0, s5 = 0, s6 = GRIPPER_OPEN },
      [3] = { velocity = 5, type = SERVO_R, s1 = 0, s2 = -190, s3 = 0, s4 = 0, s5 = 0, s6 = 0 },
      [4] = { velocity = 5, type = SERVO_ABSOLUTE, s1 = 0, s2 = 0, s3 = 0, s4 = 0, s5 = 0, s6 = 2470 },
