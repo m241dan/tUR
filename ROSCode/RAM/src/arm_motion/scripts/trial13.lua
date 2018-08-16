1235b68091ec2d74e63bc571b340a7bebdc75a29
Daniel R. Koris <korisd@gmail.com>
2018-08-16 14:03:42 -0400

    trying a left turn

---
+++ ROSCode/RAM/src/arm_motion/scripts/trial13.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: korisd
+-- Date: 8/16/18
+-- Time: 2:02 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+require( 'types' )
+require( 'motions' )
+
+trial = {}
+
+addObjective( trial, upper_pot_left_turn )
+
+return trial
+
---
+++ ROSCode/RAM/src/arm_motion/scripts/trial14.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: korisd
+-- Date: 8/16/18
+-- Time: 2:02 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+require( 'trial' )
+require( 'motion' )
+
+trial = {}
+
+addObjective( trial, lower_pot_left_turn )
+
+return trial
+
