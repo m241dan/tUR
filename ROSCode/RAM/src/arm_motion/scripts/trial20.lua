930ac1e91d3f94a8182576785fbb31b908630778
Daniel R. Koris <korisd@gmail.com>
2018-08-16 14:40:02 -0400

    first macro trial

---
+++ ROSCode/RAM/src/arm_motion/scripts/trial20.lua
@@ -0,0 +1,27 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: korisd
+-- Date: 8/16/18
+-- Time: 2:38 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+require( 'types' )
+require( 'motions' )
+
+trial = {}
+
+addObjective( trial, blu_button )
+addObjective( trial, right_switch_down_to_up )
+addObjective( trial, left_switch_down_to_up )
+addObjective( trial, right_switch_up_to_down )
+addObjective( trial, left_switch_down_to_up )
+addObjective( trial, blu_button )
+addObjective( trial, idle )
+addObjective( trial, upper_pot_right_turn )
+addObjective( trial, upper_pot_left_turn )
+addObjective( trial, lower_pot_right_turn )
+addObjective( trial, lower_pot_left_turn )
+
+return trial
+
