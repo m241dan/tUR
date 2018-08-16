bb62266e609752d2c90f88a9ef5912fe67dd2504
Daniel R. Koris <korisd@gmail.com>
2018-08-16 13:15:47 -0400

    forgot to add motions

---
+++ ROSCode/RAM/src/arm_motion/scripts/motions.lua
@@ -0,0 +1,9 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: korisd
+-- Date: 8/16/18
+-- Time: 1:12 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+blu_button = require( "blu_button" )
