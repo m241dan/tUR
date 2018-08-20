ce5ff69333f058eb7516cf220d273b5a43c11112
Daniel R. Koris <korisd@gmail.com>
2018-08-20 15:30:31 -0400

    added two new trials

---
+++ ROSCode/RAM/src/arm_motion/scripts/trial21.lua
@@ -0,0 +1,27 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: korisd
+-- Date: 8/20/18
+-- Time: 3:29 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+require( 'types' )
+require( 'motions' )
+
+trial = {}
+
+addObjective( trial, blu_button )
+addObjective( trial, upper_pot_right_turn )
+addObjective( trial, upper_pot_left_turn )
+addObjective( trial, lower_pot_right_turn )
+addObjective( trial, lower_pot_left_turn )
+addObjective( trial, blu_button )
+addObjective( trial, upper_pot_right_turn )
+addObjective( trial, upper_pot_left_turn )
+addObjective( trial, lower_pot_right_turn )
+addObjective( trial, lower_pot_left_turn )
+addObjective( trial, idle )
+
+
+return trial
---
+++ ROSCode/RAM/src/arm_motion/scripts/trial22.lua
@@ -0,0 +1,25 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: korisd
+-- Date: 8/20/18
+-- Time: 3:29 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+require( 'types' )
+require( 'motions' )
+
+trial = {}
+
+addObjective( trial, right_switch_down_to_up )
+addObjective( trial, left_switch_down_to_up )
+addObjective( trial, right_switch_up_to_down )
+addObjective( trial, left_switch_up_to_down )
+addObjective( trial, idle )
+addObjective( trial, right_switch_down_to_up )
+addObjective( trial, left_switch_down_to_up )
+addObjective( trial, right_switch_up_to_down )
+addObjective( trial, left_switch_up_to_down )
+addObjective( trial, idle )
+
+return trial
